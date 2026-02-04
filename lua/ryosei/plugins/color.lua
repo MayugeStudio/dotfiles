23f3859faff5f65d46886f20841dd997e0d55a4b
MayugeStudio <my.favorite.is.energy@gmail.com>
2026-02-04 18:11:54 +0900

    Change color scheme

--- lua/ryosei/plugins/color.lua
+++ lua/ryosei/plugins/color.lua
@@ -1,11 +1,20 @@
+-- return {
+--   "vague2k/vague.nvim",
+--  config = function()
+--    require("vague").setup({
+--        italic = false,
+--        transparent = true,
+--    })
+--    vim.cmd("colorscheme slate")
+--    vim.cmd(":hi statusline guibg=NONE")
+--  end
+--}
+
 return {
-  "vague2k/vague.nvim",
+  "dracula/vim",
   config = function()
-    require("vague").setup({
-        italic = false,
-        transparent = true,
-    })
-    vim.cmd("colorscheme vague")
+    vim.g.dracula_italic = false
+    vim.cmd("colorscheme dracula")
     vim.cmd(":hi statusline guibg=NONE")
   end
 }
