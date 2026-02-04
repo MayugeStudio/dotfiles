7c41763f995ae9cb4a9a557e72f5680aedc8c4d1
MayugeStudio <my.favorite.is.energy@gmail.com>
2026-02-04 18:10:53 +0900

    Introduce comment-nvim

---
+++ lua/ryosei/plugins/comment.lua
@@ -0,0 +1,16 @@
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+return {
+    'numToStr/Comment.nvim',
+    opts = {},
+    config = function() 
+      require('Comment').setup({
+        toggler = {
+          line = 'gcc',
+          block = 'gbc',
+        },
+        mappings = {
+          basic = true,
+        }
+      })
+    end
+}
