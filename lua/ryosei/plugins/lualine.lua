2eecd3fbd0836a0bee3f732580e469a13e3cd3b4
MayugeStudio <my.favorite.is.energy@gmail.com>
2026-02-04 18:30:05 +0900

    Introduce lualine.nvim

---
+++ lua/ryosei/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  config = function()
+    require('lualine').setup({
+      options = {
+        icons_enabled = false,
+        theme = 'dracula',
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch'},
+        lualine_c = {'filename'},
+        lualine_x = {'filetype'},
+        lualine_y = {'encoding'},
+        lualine_z = {'location'}
+      },
+    })
+  end
+}
+
